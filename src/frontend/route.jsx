86e0bb62f1ea883dfd8f66c9f8beaeeeac237a2d
crabbbb <xuezhanixi666@gmail.com>
2024-12-09 22:57:26 +0800

    navigation bar

---
+++ src/frontend/route.jsx
@@ -0,0 +1,10 @@
+// react page link ( not for backend request )
+const routes = {
+    home: "/",
+    schedule: "/schedule",
+    bus: "/bus",
+    busStops: "/bus-stops",
+    superadmin: "http://127.0.0.1:8000/admin/",
+};
+
+export default routes;
--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { NextUIProvider } from '@nextui-org/react';
+import {useTheme} from "@nextui-org/use-theme";
+
+// to let the theme follow the user settings  
+function RootComponent() {
+  // use hock - must inside function 
+  const { theme } = useTheme();
+
+  return (
+      <main className={`${theme} light text-foreground bg-background w-screen h-screen p-4`}>
+        <App />
+      </main>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* wrap with app with using nextui */}
+    <NextUIProvider>
+        <RootComponent />
+    </NextUIProvider>
   </StrictMode>,
 )
